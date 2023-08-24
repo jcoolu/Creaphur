d7a4d923936ea7a2f92bdc365123fbf174a3d500
Jamie Coulombe <jcoulombe213@gmail.com>
2023-08-23 21:44:08 -0400

    updated colorScheme for app

--- creaphur/lib/main.dart
+++ creaphur/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,22 +14,22 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Creaphur',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running yourß application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        textTheme: GoogleFonts.latoTextTheme(Theme.of(context).textTheme),
+        colorScheme: const ColorScheme(
+          brightness: Brightness.light,
+          primary: Color(4285286399),
+          onPrimary: Color(4280877260),
+          secondary: Color(4281059952),
+          onSecondary: Color(4280125259),
+          error: Color(0xFFF32424),
+          onError: Color(0xFFF32424),
+          background: Color(0xFFF1F2F3),
+          onBackground: Color(0xFFFFFFFF),
+          surface: Color(4285286399),
+          onSurface: Color(4280877260),
+          primaryContainer: Color(4289567231),
+          secondaryContainer: Color(4287227569),
+        ),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),

9684d5acc6479f574cf63cc196f57bccd5ffbbce
Jamie Coulombe <jcoulombe213@gmail.com>
2023-10-13 21:20:41 -0400

    added dashboard

---
+++ creaphur/lib/screens/dashboard.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class Dashboard extends StatelessWidget {
+  const Dashboard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          onPressed: null,
+        ),
+        title: const Text('Welcome, {Name}'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.person_2_rounded),
+            onPressed: null,
+          ),
+        ],
+      ),
+      body: SafeArea(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 16),
+            child: Text(
+                'No projects found. Please click (+) below to create a new project.'),
+          ),
+        ),
+      ),
+      floatingActionButton: ElevatedButton(
+        child: Icon(Icons.add),
+        onPressed: null,
+        style: ButtonStyle(
+          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+            RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20.0),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}

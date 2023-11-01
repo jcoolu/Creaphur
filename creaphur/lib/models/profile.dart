50c7bfee0099003ada9e2e89281e81b3e168b74b
Jamie Coulombe <jcoulombe213@gmail.com>
2023-11-01 14:32:55 -0400

    added project and profile info

---
+++ creaphur/lib/models/profile.dart
@@ -0,0 +1,15 @@
+class Profile {
+  late String id;
+  late String name;
+
+  Profile(this.id, this.name);
+
+  // to-do later
+  int getNumberOfProjects() => 0;
+
+  //to-do later
+  String getMaterialFunInfo() => 'You have used 3 yards of fabric';
+
+  // to-do later
+  String getMotivationQuote() => 'Keep  Moving Forward';
+}
---
+++ creaphur/lib/models/project.dart
@@ -0,0 +1,12 @@
+class Project {
+  late String id;
+  late String name;
+  late String description;
+  late DateTime startDate;
+  late DateTime endDate;
+  late double estCost;
+  late String ownerId;
+
+  Project(this.id, this.name, this.description, this.startDate, this.endDate,
+      this.estCost, this.ownerId);
+}

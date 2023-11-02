414e9f0f2f1e740048dac4b6476f90611302804d
Jamie Coulombe <jcoulombe213@gmail.com>
2023-11-02 12:07:13 -0400

    added expense model

---
+++ creaphur/lib/models/expense.dart
@@ -0,0 +1,10 @@
+class Expense {
+  late String id;
+  late String materialId;
+  late String projectId;
+  late double quantity;
+  late String quantityType;
+
+  Expense(this.id, this.materialId, this.projectId, this.quantity,
+      this.quantityType);
+}

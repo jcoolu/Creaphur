47599135d7f27a8cc953b2c545c4214611458a1e
Jamie Coulombe <jcoulombe213@gmail.com>
2023-11-06 23:14:33 -0500

    added color table comment to theme.dart

--- creaphur/lib/common/theme.dart
+++ creaphur/lib/common/theme.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+/*
+Colors for reference:
+          Color(4285286399), - main purple
+          Color(4280877260), - dark purple
+          Color(4281059952), - main green
+          Color(4280125259), - dark green
+          Color(4289567231), - light purple
+          Color(4287227569), - light green
+*/
 ThemeData appTheme(BuildContext context) => ThemeData(
       textTheme: GoogleFonts.latoTextTheme(Theme.of(context).textTheme),
       colorScheme: const ColorScheme(

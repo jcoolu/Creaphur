a440248f90e978914635ed40c041923579bc0639
Jamie Coulombe <jcoulombe213@gmail.com>
2023-11-06 23:16:32 -0500

    added providers to main.dart page for running app

--- creaphur/lib/main.dart
+++ creaphur/lib/main.dart
@@ -1,107 +1,33 @@
+import 'package:creaphur/models/expense_list.dart';
+import 'package:creaphur/models/material_list.dart';
+import 'package:creaphur/models/profile_list.dart';
+import 'package:creaphur/models/project_list.dart';
+import 'package:creaphur/models/time_entry_list.dart';
 import 'package:creaphur/screens/welcome.dart';
 import 'package:creaphur/common/theme.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MultiProvider(providers: [
+    ChangeNotifierProvider<ExpenseList>(create: (context) => ExpenseList([])),
+    ChangeNotifierProvider<MaterialList>(create: (context) => MaterialList([])),
+    ChangeNotifierProvider<ProjectList>(create: (context) => ProjectList([])),
+    ChangeNotifierProvider<ProfileList>(create: (context) => ProfileList([])),
+    ChangeNotifierProvider<TimeEntryList>(
+        create: (context) => TimeEntryList([])),
+  ], child: const MyApp()));
 }
 
-/*
-Colors for reference:
-          Color(4285286399), - main purple
-          Color(4280877260), - dark purple
-          Color(4281059952), - main green
-          Color(4280125259), - dark green
-          Color(4289567231), - light purple
-          Color(4287227569), - light green
-*/
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Creaphur',
       theme: appTheme(context),
       home: const WelcomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        foregroundColor: Colors.white,
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }

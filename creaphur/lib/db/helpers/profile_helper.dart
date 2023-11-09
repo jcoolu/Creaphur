4071e2d7f322ed5ee144954eb05fe55af2cfb182
Jamie Coulombe <jcoulombe213@gmail.com>
2023-11-09 11:55:33 -0500

    added profile helper

---
+++ creaphur/lib/db/helpers/profile_helper.dart
@@ -0,0 +1,42 @@
+import 'package:creaphur/db/main.dart';
+import 'package:creaphur/models/profile.dart';
+import 'package:sqflite/sqflite.dart';
+
+class ProfileHelper {
+  static Future<void> addProfile(Profile profile) async {
+    final db = await DatabaseHelper.getDB();
+
+    await db.insert(
+      'Profile',
+      profile.toMap(),
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
+  }
+
+  static Future<void> updateProfile(Profile profile) async {
+    final db = await DatabaseHelper.getDB();
+
+    await db.update("Profile", profile.toMap(),
+        where: 'id = ?',
+        whereArgs: [profile.id],
+        conflictAlgorithm: ConflictAlgorithm.replace);
+  }
+
+  static Future<void> deleteProfile(Profile profile) async {
+    final db = await DatabaseHelper.getDB();
+
+    await db.delete("Profile", where: 'id = ?', whereArgs: [profile.id]);
+  }
+
+  static Future<List<Profile>?> getProfiles() async {
+    final db = await DatabaseHelper.getDB();
+
+    final List<Map<String, dynamic>> maps = await db.query("Profile");
+
+    if (maps.isEmpty) {
+      return null;
+    }
+
+    return List.generate(maps.length, (index) => Profile.fromMap(maps[index]));
+  }
+}

d25a8b6ee181ea77559139ac8cfab9cc73a39388
Jamie Coulombe <jcoulombe213@gmail.com>
2023-11-10 11:54:42 -0500

    added profile service for add and get

---
+++ creaphur/lib/services/profile_service.dart
@@ -0,0 +1,16 @@
+import 'package:creaphur/db/helpers/profile_helper.dart';
+import 'package:creaphur/models/profile.dart';
+import 'package:creaphur/models/profile_list.dart';
+import 'package:provider/provider.dart';
+
+class ProfileService {
+  static Future<void> addProfile(context, Profile profile) async {
+    await ProfileHelper.addProfile(profile);
+    Provider.of<ProfileList>(context, listen: false).add(profile);
+  }
+
+  static getProfiles(context) async {
+    List<Profile> profiles = await ProfileHelper.getProfiles() ?? [];
+    Provider.of<ProfileList>(context).addAllProfiles(profiles);
+  }
+}

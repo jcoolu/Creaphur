7f11992fed12750d7c06ef9f78eb5e2bce33a46a
Jamie Coulombe <jcoulombe213@gmail.com>
2023-11-15 10:48:06 -0500

    added date and time pickers for start and end date for project

--- creaphur/lib/widgets/forms/project_form.dart
+++ creaphur/lib/widgets/forms/project_form.dart
@@ -1,10 +1,8 @@
-import 'package:creaphur/models/expense_list.dart';
+import 'dart:async';
+
 import 'package:creaphur/models/project.dart';
+import 'package:creaphur/widgets/date_time_picker.dart';
 import 'package:flutter/material.dart';
-import 'package:multi_select_flutter/dialog/multi_select_dialog_field.dart';
-import 'package:multi_select_flutter/util/multi_select_item.dart';
-import 'package:multi_select_flutter/util/multi_select_list_type.dart';
-import 'package:provider/provider.dart';
 
 // Define a custom Form widget.
 class ProjectForm extends StatefulWidget {
@@ -18,19 +16,62 @@ class ProjectForm extends StatefulWidget {
   }
 }
 
-// Define a corresponding State class.
-// This class holds data related to the form.
 class ProjectFormState extends State<ProjectForm> {
   final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
-    List<MultiSelectItem> expenses =
-        Provider.of<ExpenseList>(context, listen: true)
-            .items
-            .where((e) => e.projectId == widget.project.id)
-            .map((e) => MultiSelectItem(e, e.name))
-            .toList();
+    void handleChangeStartDate(field, value) {
+      if (field == 'date') {
+        DateTime nextDate = value;
+        widget.onChange(
+            'startDate',
+            DateTime(
+              nextDate.year,
+              nextDate.month,
+              nextDate.day,
+              widget.project.startDate.hour,
+              widget.project.startDate.minute,
+            ));
+      } else {
+        TimeOfDay nextTime = value;
+        widget.onChange(
+            'startDate',
+            DateTime(
+              widget.project.startDate.year,
+              widget.project.startDate.month,
+              widget.project.startDate.day,
+              nextTime.hour,
+              nextTime.minute,
+            ));
+      }
+    }
+
+    void handleChangeEndDate(field, value) {
+      if (field == 'date') {
+        DateTime nextDate = value;
+        widget.onChange(
+            'endDate',
+            DateTime(
+              nextDate.year,
+              nextDate.month,
+              nextDate.day,
+              widget.project.startDate.hour,
+              widget.project.startDate.minute,
+            ));
+      } else {
+        TimeOfDay nextTime = value;
+        widget.onChange(
+            'endDate',
+            DateTime(
+              widget.project.startDate.year,
+              widget.project.startDate.month,
+              widget.project.startDate.day,
+              nextTime.hour,
+              nextTime.minute,
+            ));
+      }
+    }
 
     return Padding(
       padding: const EdgeInsets.all(12),
@@ -67,36 +108,14 @@ class ProjectFormState extends State<ProjectForm> {
                     return null;
                   },
                 ),
-                TextFormField(
-                  // need to change to date time widget
-                  initialValue: widget.project.startDate.toString(),
-                  decoration: const InputDecoration(
-                    hintText: 'Projected Start Date for Project',
-                    labelText: 'Projected Start Date *',
-                  ),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter a projected start date for your project';
-                    }
-                    widget.onChange('startDate', value);
-                    return null;
-                  },
-                ),
-                TextFormField(
-                  // need to change to date time widget
-                  initialValue: widget.project.endDate.toString(),
-                  decoration: const InputDecoration(
-                    hintText: 'Projected End Date for Project',
-                    labelText: 'Projected End Date *',
-                  ),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter a projected end date for your project';
-                    }
-                    widget.onChange('endDate', value);
-                    return null;
-                  },
-                ),
+                DateTimePicker(
+                    dateTime: widget.project.startDate,
+                    onChange: handleChangeStartDate,
+                    buttonText: 'Projected Start'),
+                DateTimePicker(
+                    dateTime: widget.project.endDate,
+                    onChange: handleChangeEndDate,
+                    buttonText: 'Projected End'),
                 TextFormField(
                   initialValue: widget.project.estCost.toString(),
                   keyboardType:

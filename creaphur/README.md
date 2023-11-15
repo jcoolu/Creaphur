6ea73a85c6ac48847f4151c6fa846f7d3b627625
Jamie Coulombe <jcoulombe213@gmail.com>
2023-11-15 15:09:58 -0500

    added widgetbook

--- creaphur/README.md
+++ creaphur/README.md
@@ -14,3 +14,7 @@ A few resources to get you started if this is your first Flutter project:
 For help getting started with Flutter development, view the
 [online documentation](https://docs.flutter.dev/), which offers tutorials,
 samples, guidance on mobile development, and a full API reference.
+
+## How to Start Widgetbook
+
+bash flutter run -d macos -t lib/widgetbook.dart
---
+++ creaphur/lib/widgetbook.dart
@@ -0,0 +1,19 @@
+// Widgetbook file: widgetbook.dart
+import 'package:flutter/material.dart';
+import 'package:widgetbook/widgetbook.dart';
+
+void main() {
+  runApp(const HotReload());
+}
+
+class HotReload extends StatelessWidget {
+  const HotReload({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Widgetbook.material(
+      addons: [],
+      directories: [],
+    );
+  }
+}

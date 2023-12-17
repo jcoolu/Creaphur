b72e2f2c25e4aabf64fba351614cf294c9c44373
Jamie Coulombe <jcoulombe213@gmail.com>
2023-12-17 01:53:55 -0500

    added check for quantity

--- creaphur/lib/common/utils.dart
+++ creaphur/lib/common/utils.dart
@@ -4,6 +4,11 @@ class Utils {
     return regExp.hasMatch(value);
   }
 
+  static isQuantityValid(String value) {
+    RegExp regExp = RegExp(r'^\d+(\.\d+)?$');
+    return regExp.hasMatch(value);
+  }
+
   static int daysBetween(DateTime from, DateTime to) {
     from = DateTime(from.year, from.month, from.day);
     to = DateTime(to.year, to.month, to.day);
